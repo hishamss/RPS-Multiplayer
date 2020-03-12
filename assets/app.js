85393deb76261e26d5bce179d69b0ff02af5a4e3
hishamss <hishamss90@gmail.com>
2020-03-11 23:35:47 -0700

    created the requested files and added the links to Firebase and Jquery

---
+++ assets/app.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDDuqGDPv8cO-4S-I7YPJt07qpmCFf_cj4",
+  authDomain: "rps-multiplayer-445bf.firebaseapp.com",
+  databaseURL: "https://rps-multiplayer-445bf.firebaseio.com",
+  projectId: "rps-multiplayer-445bf",
+  storageBucket: "rps-multiplayer-445bf.appspot.com",
+  messagingSenderId: "817299326611",
+  appId: "1:817299326611:web:bfc3ce96baab5deed3ef66"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
